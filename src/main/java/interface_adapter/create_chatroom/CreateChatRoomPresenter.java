8025eed182b9fdea41c5e25c2a1a68810e1c9479
belarbic <}belarbic@users.noreply.github.com>
2024-11-15 21:38:42 -0500

    edited createchatroompresenter

--- src/main/java/interface_adapter/create_chatroom/CreateChatRoomPresenter.java
+++ src/main/java/interface_adapter/create_chatroom/CreateChatRoomPresenter.java
@@ -5,11 +5,9 @@ import interface_adapter.ViewManagerModel;
 import interface_adapter.change_password.LoggedInState;
 import interface_adapter.change_password.LoggedInViewModel;
 import use_case.create_chatroom.CreateChatRoomOutputBoundary;
-import use_case.create_chatroom.CreateChatRoomOutputData;
 import use_case.login.LoginOutputBoundary;
 import use_case.login.LoginOutputData;
 */
-
 /*
  * The Presenter for the Create ChatRoom Use Case.
  */
@@ -17,36 +15,36 @@ import use_case.login.LoginOutputData;
 public class CreateChatRoomPresenter implements CreateChatRoomOutputBoundary {
 
     private final CreateChatRoomViewModel createChatRoomViewModel;
-    // private final ChatRoomViewModel chatRoomViewModel;
+    private final ChatRoomViewModel chatRoomViewModel;
     private final ViewManagerModel viewManagerModel;
 
-    public CreateChatRoomPresenter(ViewManagerModel viewManagerModel,
-                                   // ChatRoomViewModel chatRoomViewModel,
-                                   CreateChatRoomViewModel createChatRoomViewModel) {
+    public LoginPresenter(ViewManagerModel viewManagerModel,
+                          ChatRoomViewModel chatRoomViewModel,
+                          CreateChatRoomViewModel createChatRoomViewModel) {
         this.viewManagerModel = viewManagerModel;
-        // this.chatRoomViewModel = chatRoomViewModel;
+        this.chatRoomViewModel = chatRoomViewModel;
         this.createChatRoomViewModel = createChatRoomViewModel;
     }
 
     @Override
-    public void prepareSuccessView(CreateChatRoomOutputData response) {
-        // On success, switch to the chat room view.
+    public void prepareSuccessView(LoginOutputData response) {
+        // On success, switch to the logged in view.
 
-        // final ChatRoomState chatRoomState = chatRoomViewModel.getState();
-        // fix this
-        // chatRoomState.setUsername(response.getUsername());
-        // this.chatRoomViewModel.setState(chatRoomState);
-        // this.chatRoomViewModel.firePropertyChanged();
+        final LoggedInState loggedInState = chatRoomViewModel.getState();
+        loggedInState.setUsername(response.getUsername());
+        this.chatRoomViewModel.setState(loggedInState);
+        this.chatRoomViewModel.firePropertyChanged();
 
-        // this.viewManagerModel.setState(chatRoomViewModel.getViewName());
-        // this.viewManagerModel.firePropertyChanged();
+        this.viewManagerModel.setState(chatRoomViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
     }
 
     @Override
-     public void prepareFailView(String error) {
-        final CreateChatRoomState createChatRoomState = createChatRoomViewModel.getState();
-        createChatRoomState.setLoginError(error);
+    public void prepareFailView(String error) {
+        final LoginState loginState = createChatRoomViewModel.getState();
+        loginState.setLoginError(error);
         createChatRoomViewModel.firePropertyChanged();
     }
 }
 */
+// waiting on the creation of the chat room view model
