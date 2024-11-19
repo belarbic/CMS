c85a3361581ea6ac0ba62c2a690d5a377a88b7ac
belarbic <}belarbic@users.noreply.github.com>
2024-11-18 23:44:42 -0500

    edited most of the create chatRoom use cases

--- src/main/java/use_case/create_chatroom/CreateChatRoomUserDataAccessInterface.java
+++ src/main/java/use_case/create_chatroom/CreateChatRoomUserDataAccessInterface.java
@@ -1,11 +1,23 @@
 package use_case.create_chatroom;
 
-import entity.User;
+import entity.ChatRoom;
 
 /**
- * The interface of the DAO for the Create ChatRoom Use Case.
+ * DAO for the Create ChatRoom Use Case.
  */
 public interface CreateChatRoomUserDataAccessInterface {
 
-    // TODO
+    /**
+     * Checks if the given chatRoom exists.
+     * @param name the name to look for
+     * @return true if a chatRoom with this name already exists.
+     */
+    boolean existsByName(String name);
+
+    /**
+     * Saves the ChatRoom.
+     * @param  chatRoom the chatRoom to save
+     */
+    void save(ChatRoom chatRoom);
+
 }
